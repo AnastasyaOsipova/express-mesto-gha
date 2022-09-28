c43a6a374972df1f9c4407403eaef21e267c9f40
AnastasyaOsipova <nastasjaosipova@yandex.ru>
2022-09-28 22:05:18 +0300

    tests

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().regex(authRegex),
+    avatar: Joi.string().regex(authRegex),
   }),
 }), createUser);
 
